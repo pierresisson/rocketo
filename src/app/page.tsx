f8ce2e978efd9f971de7c89bcf44e4fd8b34dc6e
pierresisson <79455860+pierresisson@users.noreply.github.com>
2024-09-11 22:08:37 +0200

    hello rocketo.io :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,27 @@
-"use client"
+"use client";
 
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { CommandDialog, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem } from "@/components/ui/command"
-import { Button } from "@/components/ui/button"
-import { MoonIcon, SunIcon, SearchIcon, MenuIcon } from "lucide-react"
-import { motion, AnimatePresence } from "framer-motion"
-import LandingPage from '@/components/Landing'
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import LandingPage from "@/components/Landing";
 
 export default function Home({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState('dark')
-  const [isOpen, setIsOpen] = useState(false)
-  const router = useRouter()
+	const [theme] = useState("dark");
 
-  
-
-  return (
-    <div className={`min-h-screen bg-background text-foreground ${theme}`}>
-      <main className="container py-6 md:py-10">
-        <AnimatePresence mode="wait">
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.2 }}
-          >
-            {children}
-          </motion.div>
-        </AnimatePresence>
-      </main>
-      <LandingPage />
-    </div>
-  )
+	return (
+		<div className={`bg-background text-foreground ${theme}`}>
+			<main className="container py-6 md:py-10">
+				<AnimatePresence mode="wait">
+					<motion.div
+						initial={{ opacity: 0, y: 20 }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: -20 }}
+						transition={{ duration: 0.2 }}
+					>
+						{children}
+					</motion.div>
+				</AnimatePresence>
+			</main>
+			<LandingPage />
+		</div>
+	);
 }
